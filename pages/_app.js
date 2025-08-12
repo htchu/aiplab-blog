08088b434ee7cae90b6ac7ed72d14cb6c62c6462
Hsueh-Ting Chu <htchu.taiwan@gmail.com>
2025-08-12 23:44:21 +0800

    3 commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { LanguageProvider } from '../lib/i18n';
+import * as gtag from '../lib/gtag';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <LanguageProvider>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-77HDCV7MDM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-77HDCV7MDM');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
